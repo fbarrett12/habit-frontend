bfd2dccb244715a6c87835e981f4c91fd76690e4
FredrickBarrett <f.barrett12@gmail.com>
2019-12-26 18:15:52 -0500

    changed background color

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from './Routes'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
 function App() {
